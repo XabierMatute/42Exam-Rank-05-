6a9c7d329a4c902a42327651c97a1e6a5c6155cd
XabierMatute <109271435+XabierMatute@users.noreply.github.com>
2023-10-03 18:06:04 +0200

    Add files via upload

--- Warlock.hpp
+++ Warlock.hpp
@@ -1,6 +1,10 @@
 #include <string>
 using std::string;
 
+#include <iostream>
+using std::cout;
+using std::endl;
+
 class Warlock
 {
 private:
@@ -14,8 +18,13 @@ public:
 	void	setTitle(string to_set) {title = to_set;}
 
 private:
-	Warlock(): name(), title() {;}
+	Warlock(): name(), title() {cout << name << ": This looks like another boring day." << endl;}
 	Warlock(const Warlock &to_copy): name(to_copy.name), title(to_copy.title) {;}
-	Warlock	&operator=(const Warlock &to_assing) {name = to_assing.name; title = to_assing.title;}
-	~Warlock() {;}
+	Warlock	&operator=(const Warlock &to_assing) {name = to_assing.name; title = to_assing.title; return (*this);}
+
+public:
+	Warlock(string name, string title): name(name), title(title) {cout << name << ": This looks like another boring day." << endl;;}
+	~Warlock() {cout << name << ": My job here is done!" << endl;}
+
+	void introduce() const {cout << name << ": I am " << name << ", " << title << "!" << endl;}
 };
